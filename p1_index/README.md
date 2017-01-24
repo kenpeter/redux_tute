39e4de6d746026f5345c6145654c0bb9effe4dd7
kenpeter <figo2476@hotmail.com>
2017-01-24 23:08:27 +1100

    init

---
+++ p1_index/README.md
@@ -0,0 +1,12 @@
+## Intro
+
+Tutorial based on: https://www.youtube.com/watch?v=5oiXG9f6GO0&list=PLuNEz8XtB51K-x3bwCC9uNM_cxXaiCcRY
+
+Part 1
+
+## What I learned
+
+nodemon --watch server --exec babel-node -- server/index.js
+nodemon can watch server directory
+nodemon can exec babel-node
+put -- allows nodemon run file server/index.js
---
+++ p1_index/server/index.js
@@ -0,0 +1,19 @@
+// need to have babel-cli, as it doesn't understand import
+import express from "express";
+
+// from path
+import path from "path";
+
+// const????
+const app = express();
+
+// app.get 
+// () => {}
+// req, res
+app.get('/*', (req, res) => {
+	const indexPath = path.join(__dirname, "./index.html");
+	res.sendFile(indexPath);
+});
+
+app.listen(3000, () => console.log("running localhost 3000"));
+
